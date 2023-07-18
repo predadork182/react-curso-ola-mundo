379bd29b256d0767a9ef2e1b50da357062a16b92
mikek182 <marcosvinicius@geekwork.com.br>
2023-07-18 20:03:03 -0300

    Modifiquei o olá mundo

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Olá Mundo</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,8 +16,8 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
+    </div> 
+  ); 
 }
 
 export default App;
